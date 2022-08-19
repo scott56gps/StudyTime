d1b2103ad2702f8bf251d8b0200e703402c3618d
Scott Nicholes <scott93gps@gmail.com>
2022-08-18 22:18:10 -0700

    Initial Commit

---
+++ StudyTime/StudyTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudyTimeApp.swift
+//  StudyTime
+//
+//  Created by Scott Nicholes on 8/18/22.
+//
+
+import SwiftUI
+
+@main
+struct StudyTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
